2e045abfe132f43b03626aebb427b74740a4ca30
Ryan White <mrwhiterk@gmail.com>
2019-04-08 10:34:13 -0400

    setup index.html; app works

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const parser = require('body-parser');
+const cors = require('cors');
+
+// const passport = require('./config/passport')();
+// const userController = require('./controllers/user.js');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3001);
+// app.use(passport.initialize());
+// app.use(parser.json());
+// app.use(cors());
+
+// app.use('/users', userController);
+
+// app.use(require('./routes'));
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('✅  port: ' + app.get('port'));
+});
