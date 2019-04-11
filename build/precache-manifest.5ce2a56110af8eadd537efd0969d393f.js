e6f54ac807b29944f3b5599fc088802e6055c65c
Ryan White <mrwhiterk@gmail.com>
2019-04-11 18:07:20 -0400

    added build files

---
+++ build/precache-manifest.5ce2a56110af8eadd537efd0969d393f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2743d32b748b8edbe394576c9e3e12b0",
+    "url": "/static/media/couchbum3.2743d32b.jpg"
+  },
+  {
+    "revision": "82e745857a6606b5620c09a7554e6bce",
+    "url": "/static/media/couchbum2.82e74585.jpg"
+  },
+  {
+    "revision": "0ec388bcb4030ba948b42f397c0706dd",
+    "url": "/static/media/couchbum1.0ec388bc.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "afbb41934c5f0de94708",
+    "url": "/static/js/main.8bc33111.chunk.js"
+  },
+  {
+    "revision": "2e9e97ff6fe11a8e6123",
+    "url": "/static/js/2.c31e2b66.chunk.js"
+  },
+  {
+    "revision": "afbb41934c5f0de94708",
+    "url": "/static/css/main.655d29bf.chunk.css"
+  },
+  {
+    "revision": "2e9e97ff6fe11a8e6123",
+    "url": "/static/css/2.49d5869d.chunk.css"
+  },
+  {
+    "revision": "937e8bd73f03bd837ba99aa2eff4d972",
+    "url": "/index.html"
+  }
+];
